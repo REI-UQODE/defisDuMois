e6a910f1533f6bca3855e3b118a9560f4a97d293
a <a@a>
2026-04-10 09:28:27 -0400

    Testé et corrigé certains détails

--- avril2026/src/main.py
+++ avril2026/src/main.py
@@ -1,4 +1,6 @@
-from bot import Bot, testBot
+import json
+
+from bot import Bot, TestBot
 
 class Jeu:
     def __init__(self):
@@ -7,8 +9,8 @@ class Jeu:
 def main():
     bots = [
         Bot(),
-        testBot(),
-        testBot()]
+        TestBot(),
+        TestBot()]
 
     for i in range(250):
         for b in bots:
@@ -29,6 +31,27 @@ def main():
                 # else Faux et Vrai : b1.points_obtenus += 0
             
             b1.points += b1.points_obtenus
+        
+        print("Tour "+str(i))
+        print(json.dumps(
+            {
+                "Bot":{
+                    "action":bots[0].action,
+                    "points":bots[0].points,
+                    "gains":bots[0].points_obtenus
+                },
+                "TestBot1":{
+                    "action":bots[1].action,
+                    "points":bots[1].points,
+                    "gains":bots[1].points_obtenus
+                },
+                "TestBot2":{
+                    "action":bots[2].action,
+                    "points":bots[2].points,
+                    "gains":bots[2].points_obtenus
+                }
+            }, indent=2)
+            )
 
 if __name__ == "__main__":
     main()
